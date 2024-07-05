c52d467428da9408c24cb7c5da4df76f306fa140
Chandraprakash-Pandey <cp.o.pandey@gmail.com>
2024-07-06 02:14:42 +0530

    Done function

---
+++ index.js
@@ -0,0 +1,45 @@
+let userScore = 0;
+let compScore = 0;
+
+let userScoreDisplay = document.querySelector("#Userscore");
+let compScoreDisplay = document.querySelector("#compscore");
+let displayBoard = document.querySelector("#displayboard");
+
+const options = ["rock", "paper", "scissors"];
+
+function comprandom() {
+    let randomnum = Math.floor(Math.random() * 3);
+    return options[randomnum];
+}
+
+function playGame(userChoice) {
+    let compChoice = comprandom();
+
+    if (userChoice === compChoice) {
+        displayBoard.innerHTML = `It's a tie! You both chose ${userChoice}.`;
+    } else if (
+        (userChoice === "rock" && compChoice === "scissors") ||
+        (userChoice === "paper" && compChoice === "rock") ||
+        (userChoice === "scissors" && compChoice === "paper")
+    ) {
+        userScore++;
+        userScoreDisplay.innerHTML = userScore;
+        displayBoard.innerHTML = `You win! ${userChoice} beats ${compChoice}.`;
+    } else {
+        compScore++;
+        compScoreDisplay.innerHTML = compScore;
+        displayBoard.innerHTML = `You lose! ${compChoice} beats ${userChoice}.`;
+    }
+}
+
+document.querySelector("#rock").addEventListener("click", () => {
+    playGame("rock");
+});
+
+document.querySelector("#paper").addEventListener("click", () => {
+    playGame("paper");
+});
+
+document.querySelector("#scissors").addEventListener("click", () => {
+    playGame("scissors");
+});
